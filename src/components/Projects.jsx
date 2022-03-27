7909d024d2010a2f9c766c5e99f29cd8853e6f92
Amber M <amberm31@protonmail.com>
2022-03-27 18:28:51 -0500

    Add mobile responsiveness to dropdown menu

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,27 +15,36 @@ const Projects = () => {
   return (
     <>
       <div className='projectsSection'>
-        {ProjectsData.map((project, index) => (
-          <li className='projectsWrapper' key={project.id}>
-            <Link to={project.link}>
-              <Button onClick={Clicked} key={project.id}>
-                {project.title}
-              <p>{project.category}</p>
-              </Button>
-            </Link>
-          </li>
-        ))}
-        {ProjectsData.map((project, index) => (
-          <li className='projectsWrapper' key={project.id}>
-            <Link to={project.link}>
-              <Button onClick={Clicked} key={project.id}>
-                {project.title}
-              </Button>
-            </Link>
-          </li>
-        ))}
+        <div className='menuSection'>
+        <h1 className='menuSection'>Games & Animations</h1>
+          <div className='projectsLinks'>
+            {ProjectsData.map((project, index) => (
+              <li className='projectsWrapper' key={project.id}>
+                <Link to={project.link}>
+                  <Button onClick={Clicked} key={project.id}>
+                    {project.title}
+                    <p>{project.category}</p>
+                  </Button>
+                </Link>
+              </li>
+            ))}
+          </div>
         </div>
-
+        <div className='menuSection'>
+        <h1 className='menuSection'>Websites</h1>
+          <div className='websiteLinks'>
+            {ProjectsData.map((project, index) => (
+              <li className='projectsWrapper' key={project.id}>
+                <Link to={project.link}>
+                  <Button onClick={Clicked} key={project.id}>
+                    {project.title}
+                  </Button>
+                </Link>
+              </li>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
